af0bc40a2da501b55229c760e7beefe9eb4f44e5
Stanislaw Pankevich <s.pankevich@gmail.com>
2013-12-17 11:28:56 +0400

    Xcodebuild works

---
+++ DevelopmentApp/CompositeOperationsTests/CompositeOperationsTests.m
@@ -0,0 +1,34 @@
+//
+//  CompositeOperationsTests.m
+//  CompositeOperationsTests
+//
+//  Created by Stanislaw Pankevich on 17/12/13.
+//  Copyright (c) 2013 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CompositeOperationsTests : SenTestCase
+
+@end
+
+@implementation CompositeOperationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
