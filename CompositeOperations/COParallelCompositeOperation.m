28b882bdab6dbd1fd15f9a2e1567d90ee18216e1
Stanislaw Pankevich <s.pankevich@gmail.com>
2014-11-20 12:50:36 +0300

    Rewrite PCO with use of dispatch groups

--- CompositeOperations/COParallelCompositeOperation.m
+++ CompositeOperations/COParallelCompositeOperation.m
@@ -11,10 +11,8 @@
 #import "COOperation_Private.h"
 
 @interface COParallelCompositeOperation ()
-@property (readonly, nonatomic) NSArray *operations;
-@property (assign, nonatomic) NSUInteger finishedOperations;
 
-- (void)operationDidFinish:(NSOperation <COOperation> *)operation;
+@property (readonly, nonatomic) NSArray *operations;
 
 @end
 
@@ -30,77 +28,63 @@
     }
 
     _operations         = operations;
-    _finishedOperations = 0;
 
     return self;
 }
 
 - (void)main {
+    dispatch_group_t group = dispatch_group_create();
+
+    COParallelCompositeOperation *weakSelf = self;
+
     for (NSOperation <COOperation> *operation in self.operations) {
-        [operation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:NULL];
+        NSOperation <COOperation> *weakOperation = operation;
+        operation.completionBlock = ^{
+            if (weakOperation.isCancelled) {
+                [weakSelf.operations makeObjectsPerformSelector:@selector(cancel)];
+
+                [weakSelf cancel];
+            }
+
+            dispatch_group_leave(group);
+        };
+
+        dispatch_group_enter(group);
 
         dispatch_async(dispatch_get_main_queue(), ^{
             [operation start];
         });
     }
-}
 
-- (void)operationDidFinish:(NSOperation <COOperation> *)operation {
-    _finishedOperations++;
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        if (self.isCancelled == NO) {
+            NSMutableArray *results = [NSMutableArray new];
 
-    if (operation.isCancelled) {
-        [self.operations makeObjectsPerformSelector:@selector(cancel)];
+            [self.operations enumerateObjectsUsingBlock:^(NSOperation <COOperation> *operation, NSUInteger idx, BOOL *stop) {
+                results[idx] = operation.result;
+            }];
 
-        [self cancel];
-    }
-
-    if (_finishedOperations < self.operations.count) {
-        return;
-    }
-
-    if (self.isCancelled == NO) {
-        NSMutableArray *results = [NSMutableArray new];
-
-        [self.operations enumerateObjectsUsingBlock:^(NSOperation <COOperation> *operation, NSUInteger idx, BOOL *stop) {
-            results[idx] = operation.result;
-        }];
-
-        [self finishWithResult:results];
-    } else {
-        NSMutableArray *errors  = [NSMutableArray new];
-
-        [self.operations enumerateObjectsUsingBlock:^(NSOperation <COOperation> *operation, NSUInteger idx, BOOL *stop) {
-            if (operation.error) {
-                [errors addObject:operation.error];
-            }
-        }];
-
-        if (errors.count > 0) {
-            NSError *error = [NSError errorWithDomain:@"com.CompositeOperations.COParallelCompositeOperation"
-                                                 code:0
-                                             userInfo:@{ @"errors": errors }];
-
-            [self rejectWithError:error];
+            [self finishWithResult:results];
         } else {
-            [self reject];
+            NSMutableArray *errors  = [NSMutableArray new];
+
+            [self.operations enumerateObjectsUsingBlock:^(NSOperation <COOperation> *operation, NSUInteger idx, BOOL *stop) {
+                if (operation.error) {
+                    [errors addObject:operation.error];
+                }
+            }];
+
+            if (errors.count > 0) {
+                NSError *error = [NSError errorWithDomain:@"com.CompositeOperations.COParallelCompositeOperation"
+                                                     code:0
+                                                 userInfo:@{ @"errors": errors }];
+                
+                [self rejectWithError:error];
+            } else {
+                [self reject];
+            }
         }
-    }
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath
-                      ofObject:(id)object
-                        change:(NSDictionary *)change
-                       context:(void *)context {
-
-    BOOL finished = [[change valueForKey:NSKeyValueChangeNewKey] boolValue];
-
-    if (finished) {
-        [object removeObserver:self forKeyPath:keyPath];
-
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self operationDidFinish:object];
-        });
-    }
+    });
 }
 
 @end
