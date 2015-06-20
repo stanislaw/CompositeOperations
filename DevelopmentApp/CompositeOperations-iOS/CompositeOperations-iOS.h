029e661600cfe62590b730902b5d9cacfe3a5762
Stanislaw Pankevich <s.pankevich@gmail.com>
2015-06-21 01:31:25 +0300

    Create target for iOS framework

---
+++ DevelopmentApp/CompositeOperations-iOS/CompositeOperations-iOS.h
@@ -0,0 +1,19 @@
+//
+//  CompositeOperations-iOS.h
+//  CompositeOperations-iOS
+//
+//  Created by Stanislaw Pankevich on 21/06/15.
+//  Copyright (c) 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CompositeOperations-iOS.
+FOUNDATION_EXPORT double CompositeOperations_iOSVersionNumber;
+
+//! Project version string for CompositeOperations-iOS.
+FOUNDATION_EXPORT const unsigned char CompositeOperations_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CompositeOperations_iOS/PublicHeader.h>
+
+
