0d2f51e606b5445011458acbe9f2a2c9dbb1cd22
Stanislaw Pankevich <s.pankevich@gmail.com>
2015-06-21 01:38:00 +0300

    Create target for OSX framework

---
+++ DevelopmentApp/CompositeOperations-OSX/CompositeOperations-OSX.h
@@ -0,0 +1,19 @@
+//
+//  CompositeOperations-OSX.h
+//  CompositeOperations-OSX
+//
+//  Created by Stanislaw Pankevich on 21/06/15.
+//  Copyright (c) 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CompositeOperations-OSX.
+FOUNDATION_EXPORT double CompositeOperations_OSXVersionNumber;
+
+//! Project version string for CompositeOperations-OSX.
+FOUNDATION_EXPORT const unsigned char CompositeOperations_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CompositeOperations_OSX/PublicHeader.h>
+
+
---
+++ DevelopmentApp/CompositeOperations-OSXTests/CompositeOperations_OSXTests.m
@@ -0,0 +1,40 @@
+//
+//  CompositeOperations_OSXTests.m
+//  CompositeOperations-OSXTests
+//
+//  Created by Stanislaw Pankevich on 21/06/15.
+//  Copyright (c) 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface CompositeOperations_OSXTests : XCTestCase
+
+@end
+
+@implementation CompositeOperations_OSXTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
