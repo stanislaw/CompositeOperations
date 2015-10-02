ef7a13a46ffdd1e382822c6370d8d5be0759bcc7
Stanislaw Pankevich <s.pankevich@gmail.com>
2015-10-02 20:36:12 +0200

    Add initial Example target

---
+++ DevelopmentApp/Example/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Stanislaw Pankevich on 02/10/15.
+//  Copyright © 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DevelopmentApp/Example/Classes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Example
+//
+//  Created by Stanislaw Pankevich on 02/10/15.
+//  Copyright © 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
